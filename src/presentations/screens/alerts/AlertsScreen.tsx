0c3e18946852fd0fda4f8873b3fd3c94b31139a0
Javier <javier.rostagno@gmail.com>
2024-07-12 10:20:28 -0300

    faat:folders

---
+++ src/presentations/screens/alerts/AlertsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AlertsScreen = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default AlertsScreen;
